53c2bd74089f0899b5ab6b40b2ec1a85f130c235
Rijk Mulder <mulderrijk@gmail.com>
2024-03-13 23:05:22 +0100

    addedCinemachine

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject target;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject cam;
     [SerializeField] private float smoothSpeed;
     [SerializeField] private float smoothTime;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float camDistance;
+    [SerializeField] private Vector2 maxRotation;
+    private Vector3 offset;
     private Vector3 currentVel;
-    private void Update()
+    private void Start()
+    {
+        offset = transform.position - target.transform.position;
+        offset.y = 0;
+    }
+    private void LateUpdate()
     {
         // rotate cam
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         if (Mathf.Abs(x) > 0.1f || Mathf.Abs(y) > 0.1f)
         {
-            transform.RotateAround(target.transform.position, transform.up, -x * moveSpeed * Time.deltaTime);
-            transform.RotateAround(target.transform.position, transform.right, y * moveSpeed * Time.deltaTime);
-            transform.LookAt(target.transform.position);
-        }
+            transform.RotateAround(target.transform.position, Vector3.up, -x * moveSpeed * Time.deltaTime);
 
-        // set position
-        offset.x = transform.position.x - target.transform.position.x;
-        offset.z = transform.position.z - target.transform.position.z;
-        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset, ref currentVel, smoothTime, smoothSpeed);
+            if (transform.position.z > -4.9f && transform.position.z < -1.8f)
+            {
+                transform.RotateAround(target.transform.position, transform.right, y * moveSpeed * Time.deltaTime);
+            }
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset - transform.forward * camDistance, ref currentVel, smoothTime, smoothSpeed);
     }
 }
